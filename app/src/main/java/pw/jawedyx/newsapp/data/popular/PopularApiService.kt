f89a19bd3434f390fc26ab202c0653de957e4d38
jawedyx <jawedyx@gmail.com>
2019-11-04 20:10:09 +0300

    Вернул API_KEY

--- app/src/main/java/pw/jawedyx/newsapp/data/popular/PopularApiService.kt
+++ app/src/main/java/pw/jawedyx/newsapp/data/popular/PopularApiService.kt
@@ -1,11 +1,12 @@
 package pw.jawedyx.newsapp.data.popular
 
+import pw.jawedyx.newsapp.BuildConfig
 import pw.jawedyx.newsapp.data.popular.pojo.PopularResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface PopularApiService {
-    @GET("/svc/topstories/v2/{theme}.json")
+    @GET("/svc/topstories/v2/{theme}.json?api-key=${BuildConfig.API_KEY}")
     fun getPopular(@Path("theme") theme: String): Call<PopularResponse>
 }
